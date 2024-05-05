dfd5f4488a2940725e9aad4f09527a581e7ba12a
Yogesh Saini <yogeshsaini9700@gmail.com>
2024-05-05 19:55:27 +0530

    completed Pratice_Set_2

--- 21_Pratice_Set_2.js
+++ 21_Pratice_Set_2.js
@@ -1,5 +1,5 @@
 //? objects
-/*
+
 //* they are two types of object singleton and object literals
 
 //* object literals
@@ -136,10 +136,10 @@ function Addition(Num1=15,Num2=35) {
 let Num1=450
 let Num2=550
 
-// console.log(Addition(99,1));
-// console.log(Addition());
-// console.log(Addition());
-// console.log(Addition(Num1,Num2));
+console.log(Addition(99,1));
+console.log(Addition());
+console.log(Addition());
+console.log(Addition(Num1,Num2));
 
 function Values(...val){            //* hear ... act has rest parameter and it does not allow deafult initalization
   return val         //* it will return in the form of array
@@ -254,7 +254,7 @@ console.log(func1());
 
 //* javascript runs line by line
 
-*/
+
 
 //? Arrow Func and this
 
@@ -315,3 +315,286 @@ let Func7=()=>{
 let Func8=()=>(console.log("JS"),console.log(this))
 
 // Func8()
+
+
+
+//? IIFE
+
+function Work(){
+  console.log("this func is working");
+}
+
+Work()
+
+function language(lang){
+  console.log(` I am Working With ${lang} language`)
+}
+
+language("JavaScript");
+
+((l)=>{
+  console.log(` This is ${l} language`)
+})("JS");
+
+((l)=>(
+    console.log(` ${l} is also a language `)
+))("Python");
+
+(function oneMore(role){
+  console.log(` i am trying to become ${role}`);
+})("Web Developer");
+
+
+
+
+//? Control flow
+
+let Var8=true
+
+// if(Var8=="True"){
+//   console.log(" hear var 8 is true to True");
+// }
+// else if(Var8=="true"){
+//   console.log(" hear var 8 is true to true");
+// }
+// else if(Var8==1){
+//   console.log(" hear var 8 is true to 1");
+// }
+// else if(Var8=="true"&&Var8==1){
+//   console.log(" hear var 8 is true to true and 1");
+// }
+// else if(Var8=="True"||Var8==1){
+//   console.log(" hear var 8 is true to True or 1");
+// }
+// else if(Var8=="true"||Var8==0){
+//   console.log(" hear var 8 is true to true or 0");
+// }
+// else if(Var8!=="true"&&Var8!==1){
+//   console.log(" hear var 8 is not true to true or 1");
+// }
+// else if(Var8=="true"||Var8!=="true"){
+//   console.log(" hear var 8 is true to true and not equal to true with datatype");
+// }
+// else if(Var8=="1"||Var8!=="1"){
+//   console.log(" hear var 8 is true to 1 and also not equal to 1 with datatype");
+// }
+// else{
+//   console.log(" Try again Later 🙋🙂")
+// }
+
+if(Var8=="True"){
+  console.log(" hear var 8 is true to True");
+}
+if(Var8=="true"){
+  console.log(" hear var 8 is true to true");
+}
+if(Var8==1){
+  console.log(" hear var 8 is true to 1");
+}
+if(Var8=="true"&&Var8==1){
+  console.log(" hear var 8 is true to true and 1");
+}
+if(Var8=="True"||Var8==1){
+  console.log(" hear var 8 is true to True or 1");
+}
+if(Var8=="true"||Var8==0){
+  console.log(" hear var 8 is true to true or 0");
+}
+if(Var8!=="true"&&Var8!==1){
+  console.log(" hear var 8 is not true to true or 1");
+}
+if(Var8=="true"||Var8!=="true"){
+  console.log(" hear var 8 is true to true and not equal to true with datatype");
+}
+if(Var8=="1"||Var8!=="1"){
+  console.log(" hear var 8 is true to 1 and also not equal to 1 with datatype");
+}
+else{
+  console.log(" Try again Later 🙋🙂")
+}
+
+
+//* switch 
+
+let Var9="P1Y"
+
+
+switch(Var9){
+  case "JS":
+    console.log('You selected Js ');
+    break;
+  case "C":
+    console.log('You Selected C');
+    break;
+  case "GO":
+    console.log('You Selected GO');
+    break;
+  case "C++":
+    console.log('You Selected C++');
+    break;
+  case "C#":
+    console.log('You selected C#');
+    break;
+  case "PY":
+    console.log('You selected Py');
+    break;
+  default:
+    console.log(' you putted wrong values');  
+}
+
+
+
+
+
+//?loops
+
+let Var10=100
+for(let i=99;i<Var10;--i){
+      console.log(Var10,i);
+      Var10=Var10-1
+      if(Var10==0){
+        break;
+      }
+}
+
+let Var11=50
+
+while(Var11<=100){
+   console.log(((100+Var11)/2), " and ", ((100-Var11)/2))
+   
+   ++Var11
+  }
+
+let Var12=100
+
+do{
+  console.log(Var12)
+  Var12--
+  if(Var12==0){
+    break
+  }
+}while(Var12<100)  
+
+
+
+
+  //? for of loop
+
+
+// let Var13="JavaScript"
+let Arr3=[ 100, true, "Java", null, undefined, 1056566n, Symbol(554446)]  
+console.log(typeof Arr3);
+
+for ( let char of Var13){
+  console.log(char);
+}
+
+for ( let char of Arr3){
+  console.log(char, typeof char);
+}
+
+let map=new Map()
+
+console.log(map);
+
+map.set(0,100)
+map.set("a","JavaScript")
+map.set(true,false)
+map.set(null,null)
+map.set(undefined,)
+
+console.log(map);
+
+for( let char of map){
+  console.log(char, typeof char);          //* it will show key and value in array format
+}
+
+
+for (let [char] of map){        //* char will print keys
+  console.log(char, typeof char);
+}
+
+for (let [,char] of map){            //* hear char will print values beacuse we put , before 
+  console.log(char, typeof char);
+}
+
+for (let [charKey,charVal] of map){                              //* this will print  key and value sprately
+  console.log(charKey,typeof charKey ,charVal, typeof charVal);
+}
+
+
+
+let Obj6={
+  Name:"JS",
+  Days:50
+}
+
+for(let i of Obj6){
+   console.log(i);
+}
+
+for(let i in Obj6){
+  console.log(i,typeof i,",",Obj6[i],typeof Obj6[i]);
+}
+
+Arr3=[ 100, true, "Java", null, undefined, 1056566n, Symbol(554446)]  
+
+for (let i in Arr3){
+  console.log(i,Arr3[i], typeof i,typeof Arr3[i]);
+}
+
+
+Arr3.forEach((Value,Index,Array)=>{
+  console.log("<----!😄!----->");
+  console.log(Value);
+  console.log(Index);
+  console.log(Array);
+})
+
+
+
+
+function print(p){
+  console.log(p);
+}
+
+Arr3.forEach(print)
+
+
+let Arr4=[
+  {
+    Cource_Name:"JavaScript",
+    Cource_Duration:30,
+    Cource_Fees:4999,
+  },
+  {
+    Cource_Name:"Java",
+    Cource_Duration:50,
+    Cource_Fees:9999,
+  },
+  {
+    Cource_Name:"Python",
+    Cource_Duration:45,
+    Cource_Fees:8999,
+  },
+  {
+    Cource_Name:"Solidity",
+    Cource_Duration:100,
+    Cource_Fees:20000,
+  },
+  {
+    Cource_Name:"C#",
+    Cource_Duration:30,
+    Cource_Fees:4999,
+  },
+]
+
+function ObjDeatils(Obj){
+     console.log(Obj.Cource_Name);  
+     console.log(Obj.Cource_Duration);  
+     console.log(Obj.Cource_Fees);
+     console.log(` The Cource Name is ${Obj.Cource_Name} and the Cource Duration is ${Obj.Cource_Duration} Days and the Cource fees is ${Obj.Cource_Fees}`);  
+}
+
+
+Arr4.forEach(ObjDeatils)
